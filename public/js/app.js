dce259dce7c9143007d57c918c8196a90cee83da
Daniela Noir <daniela@danielanoir.com>
2016-09-02 11:12:59 -0400

    fixed js a bit. shows only one image now but it's not a random image

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,60 @@
 $(document).ready(function(){
-alert("Welcome to the Monty Hall game show! behind one door is a car and behind the other two doors are goats. You win if you choose the car. Please click a door to choose.")
+// alert("Welcome to the Monty Hall game show! behind one door is a car and behind the other two doors are goats. You win if you choose the car. Please click a door to choose.")
 
 
 var prizeImages = [$('#goatOne'), $('#goatTwo'), $('#car')]
+var currentPrize = 0;
 
-var rand = prizeImages[Math.floor(Math.random() * prizeImages.length)];
-var concat = prizeImages[rand];
-function random() {
-  document.getElementById("#doorOne").innerHTML = (concat);
-}
+$("#goatOne").hide();
+$("#goatTwo").hide();
+$("#car").hide();
 
-console.log(rand)
-
-
-
-
-$(".doorOne").on("click", function(){
-  console.log("clicked")
-  document.getElementById('goatOne').style.visibility='visible';
+$(".doorOne").click(function() {
+  $("#goatTwo").hide();
+  $("#goatOne").hide();
+  $("#car").show();
 });
 
-$(".doorTwo").on("click", function(){
-  console.log("clicked")
-  document.getElementById('goatTwo').style.visibility='visible';
-
+$(".doorTwo").click(function() {
+  $("#goatTwo").hide();
+  $("#goatOne").show();
+  $("#car").hide();
 });
-$(".doorThree").on("click", function(){
-  console.log("clicked")
-  document.getElementById('car').style.visibility='visible';
 
+$(".doorThree").click(function() {
+  $("#goatTwo").show();
+  $("#goatOne").hide();
+  $("#car").hide();
 });
+
+currentPrize = 1;
+// var prizeImages = [$('#goatOne'), $('#goatTwo'), $('#car')]
+
+// var rand = prizeImages[Math.floor(Math.random() * prizeImages.length)];
+// var concat = prizeImages[rand];
+// function random() {
+//   document.getElementById("#doorOne").innerHTML = (concat);
+// }
+//
+// console.log(rand)
+//
+//
+//
+//
+// $(".doorOne").on("click", function(){
+//   console.log("clicked")
+//   document.getElementById('goatOne').style.visibility='visible';
+// });
+//
+// $(".doorTwo").on("click", function(){
+//   console.log("clicked")
+//   document.getElementById('goatTwo').style.visibility='visible';
+//
+// });
+// $(".doorThree").on("click", function(){
+//   console.log("clicked")
+//   document.getElementById('car').style.visibility='visible';
+//
+// });
 
 });
