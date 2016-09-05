8370695438ec13a4beef9f8822d05706912a1873
Daniela Noir <daniela@danielanoir.com>
2016-09-05 17:40:24 -0400

    added angular

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 The Game:
 Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the others, goats. You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door, say No. 3, which has a goat. He then says to you, "Do you want to pick door No. 2?" Is it to your advantage to switch your choice?
+*******************************************
+PSUEDO:
+
+HTML has 3 divs representing doors. id= door1, door2, door3, class=door and isOpen, isPrize, and isSelected = 0.
+JS will change the isPrize to 1 for one random door(using id)
+first, show three doors
+user clicks one door
+onclick, set isSelected = 1
+Set isOpen=1 for one random door where isSelected=0 and isPrize=0.
+Display isOpen=1 (can be global if isOpen=1 display image)
+Prompt user reselect previous choice or click on other door where isOpen=0
+isSelected=1 for door just clicked, and 0 for other doors (so it will override previously selected)
+then isOpen=1 for all doors
 
 ****************************************
 
@@ -10,15 +23,7 @@ Websockets, Mongo, Node, Angular, Express, JavaScript, JQuery, HTML, CSS.
 bronze:
   Websockets:
     Use websockets so app is regularly updating with each users scores.
-  JS:
-    use prompts to get user answer
-    create 3 divs with classes of door 1, 2 and 3
-    create array of 2 goats and 1 car
-    attach goat or car to each div randomly from the array. user Math.random to select random picture of goat or car
-    hide attachment goat or car
-    when user clicks div, show a different card with a goat behind it.
-    ask user if they would like to change doors.
-    show result
+
 
 silver:
 create two sides: one has the percent correct for when the user switches doors and one side has percent correct when the user doesn't switch doors.
--- public/js/app.js
+++ public/js/app.js
@@ -1,60 +1,48 @@
-$(document).ready(function(){
-// alert("Welcome to the Monty Hall game show! behind one door is a car and behind the other two doors are goats. You win if you choose the car. Please click a door to choose.")
+var montyHall = angular.module('montyHall', []);
+console.log("HI");
+
+montyHall.controller('mainController', function($scope) {
+
+var Door = function(isOpen, isSelected, isPrize) {
+  this.isOpen = isOpen;
+  this.isSelected = isSelected;
+  this.isPrize = isPrize;
+};
+
+  $scope.door1 = new Door(0, 0, 0);
+  $scope.door2 = new Door(0, 0, 0);
+  $scope.door3 = new Door(0, 0, 0);
 
 
-var prizeImages = [$('#goatOne'), $('#goatTwo'), $('#car')]
-var currentPrize = 0;
-
-$("#goatOne").hide();
-$("#goatTwo").hide();
-$("#car").hide();
-
-$(".doorOne").click(function() {
-  $("#goatTwo").hide();
-  $("#goatOne").hide();
-  $("#car").show();
 });
 
-$(".doorTwo").click(function() {
-  $("#goatTwo").hide();
-  $("#goatOne").show();
-  $("#car").hide();
-});
+// $(document).ready(function(){
+//   alert("Welcome to the Monty Hall game show! behind one door is a car and behind the other two doors are goats. You win if you choose the car. Please click a door to choose.")
+//   console.log($(".door").attr("ng-isOpen"));
+//   var currentPrize = 0;
+//
+//   $("#goatOne").hide();
+//   $("#goatTwo").hide();
+//   $("#car").hide();
+//
+//   $(".door1").click(function() {
+//     $("#goatTwo").hide();
+//     $("#goatOne").hide();
+//     $("#car").show();
+//   });
+//
+//   $(".door2").click(function() {
+//     $("#goatTwo").hide();
+//     $("#goatOne").show();
+//     $("#car").hide();
+//   });
+//
+//   $(".door3").click(function() {
+//     $("#goatTwo").show();
+//     $("#goatOne").hide();
+//     $("#car").hide();
+//   });
+//
+//   currentPrize = 1;
 
-$(".doorThree").click(function() {
-  $("#goatTwo").show();
-  $("#goatOne").hide();
-  $("#car").hide();
-});
-
-currentPrize = 1;
-// var prizeImages = [$('#goatOne'), $('#goatTwo'), $('#car')]
-
-// var rand = prizeImages[Math.floor(Math.random() * prizeImages.length)];
-// var concat = prizeImages[rand];
-// function random() {
-//   document.getElementById("#doorOne").innerHTML = (concat);
-// }
-//
-// console.log(rand)
-//
-//
-//
-//
-// $(".doorOne").on("click", function(){
-//   console.log("clicked")
-//   document.getElementById('goatOne').style.visibility='visible';
 // });
-//
-// $(".doorTwo").on("click", function(){
-//   console.log("clicked")
-//   document.getElementById('goatTwo').style.visibility='visible';
-//
-// });
-// $(".doorThree").on("click", function(){
-//   console.log("clicked")
-//   document.getElementById('car').style.visibility='visible';
-//
-// });
-
-});
