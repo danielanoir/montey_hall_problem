4b4cf75e44fd9536e4c9d6777cbc0dc8966ad54a
Daniela Noir <daniela@danielanoir.com>
2016-09-05 20:35:47 -0400

    random prize door is assigned

--- public/js/app.js
+++ public/js/app.js
@@ -1,57 +1,37 @@
 var montyHall = angular.module('montyHall', []);
 montyHall.controller('mainController', function($scope) {
 
-  var Door = function(isOpen, isSelected, isPrize) {
+  var randBetween = function(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  var Door = function(doorName, isOpen, isSelected, isPrize) {
+    this.doorName = doorName;
     this.isOpen = isOpen;
     this.isSelected = isSelected;
     this.isPrize = isPrize;
   };
 
 var doorCount = 3;
+var doorList = [];
+var prizeDoor = randBetween(1,doorCount);
 
 for (var i = 1; i <= doorCount; i++) {
-    isOpen = 0;
-    isSelected = 0;
-    isPrize = 0;
-  var doorName = "door" + i.toString()
-  $scope[doorName] = new Door(isOpen,isSelected,isPrize);
-}
-
-  var openDoor = function(door) {
-    door["isOpen"] = 1;
+  var isOpen = 0;
+  var isSelected = 0;
+  var isPrize = 0;
+  if (i == prizeDoor) {
+      isPrize = 1;
   }
 
-  console.log($scope);
+  var doorName = "door" + i.toString();
+  $scope[doorName] = new Door(doorName, isOpen,isSelected,isPrize);
+  doorList.push($scope[doorName]);
+}
 
+var openDoor = function(door) {
+  door["isOpen"] = 1
+}
+
+console.log($scope);
 });
-
-// $(document).ready(function(){
-//   alert("Welcome to the Monty Hall game show! behind one door is a car and behind the other two doors are goats. You win if you choose the car. Please click a door to choose.")
-//   console.log($(".door").attr("ng-isOpen"));
-//   var currentPrize = 0;
-//
-//   $("#goatOne").hide();
-//   $("#goatTwo").hide();
-//   $("#car").hide();
-//
-//   $(".door1").click(function() {
-//     $("#goatTwo").hide();
-//     $("#goatOne").hide();
-//     $("#car").show();
-//   });
-//
-//   $(".door2").click(function() {
-//     $("#goatTwo").hide();
-//     $("#goatOne").show();
-//     $("#car").hide();
-//   });
-//
-//   $(".door3").click(function() {
-//     $("#goatTwo").show();
-//     $("#goatOne").hide();
-//     $("#car").hide();
-//   });
-//
-//   currentPrize = 1;
-
-// });
